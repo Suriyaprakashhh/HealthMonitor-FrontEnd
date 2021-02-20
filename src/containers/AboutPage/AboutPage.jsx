b09448c501ef2d185b7dd39decdfc5fe33aa5469
Wilbred Anto <79359033+Suriyaprakashhh@users.noreply.github.com>
2021-02-20 23:11:20 +0530

    first commit

---
+++ src/containers/AboutPage/AboutPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function AboutPage(){
+    return(<div className="text-center">
+        <h1 className=" mt-10 text-4xl text-blue-800">Employee Health Monitor Application</h1>
+        <h1 className="text-3xl mt-4">Developed by <span className="font-bold">Suriyaprakash.S</span></h1>
+        
+        </div>)
+}
